c6b639868337861ba226a68f0176e559f6a2f261
Ubuntu <ubuntu@ip-172-31-56-5.ec2.internal>
2016-08-02 16:42:06 +0000

    need dependencies for local development

--- public/ctrls/logInCtrl.js
+++ public/ctrls/logInCtrl.js
@@ -9,7 +9,7 @@ angular.module('logInCtrl', []).controller('LogInController', function($http, $l
             // handle success
             .then(function (res) {
                     console.log('controller function cb success');
-                    $location.path('/profile');
+                    $location.path('/board');
                     $rootScope.in = true;
                     $scope.disabled = false;
                     $scope.loginForm = {};
