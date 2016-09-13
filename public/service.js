7b72038284ca04d86e7768a183677206f6c30c2f
Leo <leooscar.simmons@gmail.com>
2016-09-13 15:42:05 -0400

    removed loggedIn() cuz it wasn't being used, and changed each function's promise structure to .then

--- public/service.js
+++ public/service.js
@@ -1,82 +1,74 @@
 angular.module('myServ', []).factory('AuthService', ['$q', '$timeout', '$http', '$location',
-	function ($q, $timeout, $http, $location) {
-	     var user = null;
+function ($q, $timeout, $http, $location) {
 
-	     function loggedIn(req, res, next){
-		 if(req.user){
-		     next();
-		 } else {
-		     res.redirect('login');
-		 }
-	     }
+  var user = null;
 
-	function isLoggedIn() {
-	    if(user) {
-		return true;
-	    } else {
-		return false;
-	    }
-	}
+  function isLoggedIn() {
+    if(user) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	function getUserStatus() {
-	    return $http.get('/app/status')
-		.success(function (data) {
-			if(data.status){
-			    user = true;
-			    return user;
-			} else {
-			    user = false;
-			    return user;
-			}
-		    })
-		.error(function (data){
-			user = false;
-			return user;
-		    });
-	}
+  function getUserStatus() {
+    return $http.get('/app/status')
+    .then(function(data){
+      if(data.authenticated){
+        user = true;
+        return user;
+      } else {
+        user = false;
+        return user;
+      }
+    }, function(data){
+      user = false;
+      return user;
+    })
+  }
 
 
-	function login(username, password) {
-	    var deferred = $q.defer();
-	    $http.post('/app/login',
-		       {username: username, password: password})
-		.success(function (data, status) {
-			if(status === 200 && data.status){
-			    user = true;
-			    deferred.resolve();
-			} else {
-			    user = false;
-			    deferred.reject();
-			}
-		    })
-		.error(function (data) {
-			user = false;
-			deferred.reject();
-		    });
-	    return deferred.promise;
+  function login(username, password) {
+    var deferred = $q.defer();
+    $http.post('/app/login',
+    {username: username, password: password})
+    .then(function(){
+      if(status === 200 && data.authenticated){
+        user = true;//for isLoggedIn()^^^
+        deferred.resolve();
+        console.log('data: ', data);
+      } else {
+        console.log('Error logging in user: ', data);
+        user = false;//for isLoggedIn()^^^
+        deferred.reject();
+      }
+    }, function(){
+      console.log('Error making /app/login request: ', error);
+      user = false;
+      deferred.reject();
+    })
+    return deferred.promise;
+  }
 
-	}
+  function logout() {
+    var deferred = $q.defer();
+    $http.get('/app/logout')
+    .then(function(){
+      user = false;
+      deferred.resolve();
+    }, function(){
+      user = false;
+      deferred.reject();
+    })
+    return deferred.promise;
+  }
 
-	function logout() {
-	    var deferred = $q.defer();
-	    $http.get('/app/logout')
-		.success(function (data) {
-			user = false;
-			deferred.resolve();
-		    })
-		.error(function (data) {
-			user = false;
-			deferred.reject();
-		    });
-	    return deferred.promise;
-	}
+  return ({
+    isLoggedIn: isLoggedIn,
+    getUserStatus: getUserStatus,
+    login: login,
+    logout: logout,
+    user: user
+  });
 
-	return ({
-		isLoggedIn: isLoggedIn,
-		    getUserStatus: getUserStatus,
-		    login: login,
-		    logout: logout,
-		    user: user
-		    });
-
-    }]);
+}]);
