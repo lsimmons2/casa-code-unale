6c325a08b14971b3e5988b43914f49f484ff3efb
Leo <leooscar.simmons@gmail.com>
2016-09-14 11:59:42 -0400

    fix auth check in getUserStatus()

--- public/ctrls/boardCtrl.js
+++ public/ctrls/boardCtrl.js
@@ -4,48 +4,49 @@ angular.module('boardCtrl', []).controller('BoardController', function($scope, $
 	$scope.skillsTOSearch = '';
 	$scope.skillsTLSearch = '';
 	$scope.open = function (userEmail) {
-	    var modalTemplate = '';
-	    AuthService.getUserStatus()
-	    .then(function(data){
-		    if(data.data.status){
-			modalTemplate = 'loggedInModal.html';
-		    } else {
-			modalTemplate = 'notLoggedInModal.html';
-		    }
-		    $uibModal.open({
-			    templateUrl: modalTemplate,
+		var modalTemplate = '';
+		AuthService.getUserStatus()
+		.then(function(data){
+			console.log('data: ', data);
+			console.log('data.data: ', data.data);
+			if(data){
+				modalTemplate = 'loggedInModal.html';
+			} else {
+				modalTemplate = 'notLoggedInModal.html';
+			}
+			$uibModal.open({
+				templateUrl: modalTemplate,
 				controller: 'ModalController',
 				resolve: {
-				userEmail: function () {
-				    return userEmail;
+					userEmail: function () {
+						return userEmail;
+					}
 				}
-			    }
 			});
 		}, function(error){
-		    console.log(error);
+		console.log(error);
 		});
-		};
+	};
 });
 
-//should make this filter part of the 'boardCtrl' module?
 angular.module('boardFilter', []).filter('orderObjectBy', function() {
 	return function(items, field, reverse) {
-	    var filtered = [];
-	    angular.forEach(items, function(item) {
-		    filtered.push(item);
+		var filtered = [];
+		angular.forEach(items, function(item) {
+			filtered.push(item);
 		});
-	    filtered.sort(function (a, b) {
-		    return (a[field] > b[field] ? 1 : -1);
+		filtered.sort(function (a, b) {
+			return (a[field] > b[field] ? 1 : -1);
 		});
-	    if(reverse) filtered.reverse();
-	    return filtered;
+		if(reverse) filtered.reverse();
+		return filtered;
 	};
-    });
+});
 
 
 angular.module('modalCtrl', []).controller('ModalController', function ($scope, $uibModalInstance, userEmail) {
 	$scope.userEmail = userEmail;
 	$scope.close = function () {
-	    $uibModalInstance.dismiss();
+		$uibModalInstance.dismiss();
 	};
-    });
+});
