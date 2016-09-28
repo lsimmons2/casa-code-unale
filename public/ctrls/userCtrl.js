474a01d18795cdf8fa1fa9023e2ab26f5361e1f6
Leo <leooscar.simmons@gmail.com>
2016-09-28 01:09:45 -0400

    filter repos by language, links for connecting github account if not authorized

--- public/ctrls/userCtrl.js
+++ public/ctrls/userCtrl.js
@@ -1,15 +1,14 @@
 angular.module('userCtrl', [
   'mgcrea.ngStrap',
   'tagsMod'
-]).controller('UserController', function($scope, $rootScope, $location, $route, $http, AuthService, $uibModal, $routeParams){
-
-  $http.get('user/' + $routeParams.username)
-  .then(function(data){
-    console.log(data);
-    $scope.user = data.data.user;
-    $scope.repos = data.data.repos;
-  }, function(data){
-    console.log('Error getting user profile');
+]).controller('UserController', function($scope, $rootScope, $filter, $location, $route, $http, AuthService, $uibModal, $routeParams){
+  $http.get('/app/user/' + $routeParams.username)
+  .then(function(resp){
+    console.log('from /user', resp);
+    $scope.user = resp.data.user;
+    console.log('repos: ', resp.data.repos);
+    $scope.repos = resp.data.repos;
+  }, function(resp){
+    console.log('Error getting user profile: ', resp);
   });
-
 });
