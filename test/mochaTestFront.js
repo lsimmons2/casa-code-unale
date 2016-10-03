703e01d6f65f798d583655b10a62f57906959cf2
Leo <leooscar.simmons@gmail.com>
2016-10-03 14:11:37 -0400

    remove old tests

--- test/mochaTestFront.js
+++
@@ -1,8 +0,0 @@
-var assert = chai.assert;
-//var chaiHttp = require('chai-http');
-
-describe('test test', function(){
-        it('A=A?', function(){
-                assert.equal('A', 'A');
-            });
-    });
